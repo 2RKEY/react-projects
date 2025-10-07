b43670bdcde1113365e3b6baaa1fa795b6a322a8
Vadim Silagin <silagin0205@gmail.com>
2025-10-07 17:55:20 +0500

    добавление отображения списка юзеров

--- src/App.js
+++ src/App.js
@@ -3,12 +3,48 @@ import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+// Берём список пользователей с https://dummyjson.com/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://dummyjson.com/users')
+      .then(res => res.json())
+      .then(json => {
+        // Преобразуем данные под старую структуру,
+        // чтобы ничего не менять в компонентах Users и User
+        const adaptedUsers = json.users.map(u => ({
+          id: u.id,
+          email: u.email,
+          first_name: u.firstName,
+          last_name: u.lastName,
+          avatar: u.image,
+        }));
+        setUsers(adaptedUsers);
+      })
+      .catch(err => {
+        console.warn(err);
+        alert('Ошибка при получении данных пользователя');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="App">
-      <Users />
+      <Users 
+        items={users}
+        isLoading={isLoading} 
+        searchValue={searchValue}
+        onChangeSearchValue={onChangeSearchValue}
+      />
       {/* <Success /> */}
     </div>
   );
